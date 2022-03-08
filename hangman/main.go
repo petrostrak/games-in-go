1b4d3636e507941623377de581d1540090e9ae7d
petrostrak <pit.trak@gmail.com>
2022-03-08 13:14:53 +0200

    Printing The Game State

--- hangman/main.go
+++ hangman/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+	"unicode"
 )
 
 var dictionary = []string{
@@ -20,9 +21,39 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 
 	targetWord := getRandomWord()
-	fmt.Println(targetWord)
+	targetWord = "United States of America"
+	guessedLetters := initGuessedWords(targetWord)
+
+	pringGameState(targetWord, guessedLetters)
+
+	guessedLetters['s'] = true
+	pringGameState(targetWord, guessedLetters)
+
+	guessedLetters['e'] = true
+	pringGameState(targetWord, guessedLetters)
+}
+
+func initGuessedWords(targetWord string) map[rune]bool {
+	guessedLetters := map[rune]bool{}
+	guessedLetters[unicode.ToLower(rune(targetWord[0]))] = true
+	guessedLetters[unicode.ToLower(rune(targetWord[len(targetWord)-1]))] = true
+
+	return guessedLetters
 }
 
 func getRandomWord() string {
 	return dictionary[rand.Intn(len(dictionary))]
+}
+
+func pringGameState(targetWord string, guessedLetters map[rune]bool) {
+	for _, ch := range targetWord {
+		if ch == ' ' {
+			fmt.Print(" ")
+		} else if guessedLetters[unicode.ToLower(ch)] {
+			fmt.Printf("%c", ch)
+		} else {
+			fmt.Print("_")
+		}
+	}
+	fmt.Println()
 }
