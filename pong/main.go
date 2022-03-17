8323df3e02eb2744bdc1b6390aa405139bec565d
petrostrak <pit.trak@gmail.com>
2022-03-17 18:32:16 +0200

    Drawing The Paddles

--- pong/main.go
+++ pong/main.go
@@ -12,8 +12,19 @@ const (
 	PaddleSymbol = 0x2588
 )
 
-func initScreen() tcell.Screen {
-	screen, err := tcell.NewScreen()
+var (
+	screen  tcell.Screen
+	player1 *Paddle
+	player2 *Paddle
+)
+
+type Paddle struct {
+	row, col, width, height int
+}
+
+func initScreen() {
+	var err error
+	screen, err = tcell.NewScreen()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
@@ -27,42 +38,59 @@ func initScreen() tcell.Screen {
 		Background(tcell.ColorBlack).
 		Foreground(tcell.ColorWhite)
 	screen.SetStyle(defStyle)
-
-	return screen
 }
 
-func Print(s tcell.Screen, row, col, width, height int, ch rune) {
+func Print(row, col, width, height int, ch rune) {
 	for r := 0; r < height; r++ {
 		for c := 0; c < width; c++ {
-			s.SetContent(col+c, row+r, ch, nil, tcell.StyleDefault)
+			screen.SetContent(col+c, row+r, ch, nil, tcell.StyleDefault)
 		}
 	}
 }
 
-func displayPaddles(screen tcell.Screen) {
+func DrawState() {
 	screen.Clear()
-	width, height := screen.Size()
-	paddleStart := height/2 - PaddleHeight/2
-	Print(screen, paddleStart, 0, 1, PaddleHeight, PaddleSymbol)
-	Print(screen, paddleStart, width-1, 1, PaddleHeight, PaddleSymbol)
+	Print(player1.row, player1.col, player1.width, player1.height, PaddleSymbol)
+	Print(player2.row, player2.col, player2.width, player2.height, PaddleSymbol)
 	screen.Show()
 }
 
 // This program just prints "Hello, World!".  Press ESC to exit.
 func main() {
-	screen := initScreen()
-	displayPaddles(screen)
+	initScreen()
+	InitGameState()
+
+	DrawState()
 
 	for {
 		switch ev := screen.PollEvent().(type) {
 		case *tcell.EventResize:
 			screen.Sync()
-			displayPaddles(screen)
+			DrawState()
 		case *tcell.EventKey:
 			if ev.Key() == tcell.KeyEnter {
 				screen.Fini()
 				os.Exit(0)
 			}
 		}
+	}
+}
+
+func InitGameState() {
+	width, height := screen.Size()
+	paddleStart := height/2 - PaddleHeight/2
+
+	player1 = &Paddle{
+		row:    paddleStart,
+		col:    0,
+		width:  1,
+		height: PaddleHeight,
+	}
+
+	player2 = &Paddle{
+		row:    paddleStart,
+		col:    width - 1,
+		width:  1,
+		height: PaddleHeight,
 	}
 }
